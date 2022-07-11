11bc716da6b5447cec1419ee09747daab7f8940f
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2022-07-11 02:14:36 +0200

    ✨ Design home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,192 @@
+@extends('layouts.master')
+
+@section('content')
+    <section class="rev_slider_wrapper ">
+        <div id="slider1" class="rev_slider" data-version="5.0">
+            <ul>
+                <li data-transition="slidingoverlayleft">
+                    <img src="img/slider/1.jpg" alt="" width="1920" height="705" data-bgposition="top center"
+                        data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="1">
+                    <div class="tp-caption sfl tp-resizeme factory-caption-h1" data-x="right" data-hoffset="0"
+                        data-y="top" data-voffset="90" data-whitespace="nowrap" data-transform_idle="o:1;"
+                        data-transform_in="o:0" data-transform_out="o:0" data-start="500">
+                        Bureau d’études en
+                        <br>Environnement et Géosciences
+                    </div>
+                    <div class="tp-caption sfl tp-resizeme factory-caption-p" data-x="right" data-hoffset="75"
+                        data-y="top" data-voffset="255" data-whitespace="nowrap" data-transform_idle="o:1;"
+                        data-transform_in="o:0" data-transform_out="o:0" data-start="500">
+                        Geoscience Congo Services propose: Les prestations intellectuelles d’ étude,
+                        <br> de conseil et d’ assistance technique, d’ expertise, de recherche documentaire
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class="our-services section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-our-service">
+                        <div class="img-box">
+                            <img src="img/our-services/1-1.jpg" alt="Awesome Image" />
+                            <div class="overlay">
+                                <div class="box">
+                                    <div class="box-content">
+                                        <a href="agricultural-engineering.html">Read More <i
+                                                class="fa fa-angle-double-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <h3>Mines et carrières</h3>
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae velit aut corrupti labore
+                            asperiores, aspernatur consequatur et accusantium ex nihil nulla fugiat similique officia
+                            aliquid, eius reprehenderit unde doloribus laudantium.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-our-service">
+                        <div class="img-box">
+                            <img src="img/our-services/1-2.jpg" alt="Awesome Image" />
+                            <div class="overlay">
+                                <div class="box">
+                                    <div class="box-content">
+                                        <a href="#">Read More <i class="fa fa-angle-double-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <h3>Contrôle environnemental</h3>
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vero laudantium iure aliquam
+                            dolorem, temporibus placeat! Numquam odit, magni quod, perferendis maiores maxime inventore vero
+                            vitae, ratione quam et voluptates.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-lg-offset-0 col-md-offset-0 col-sm-offset-3 col-xs-offset-0">
+                    <div class="single-our-service">
+                        <div class="img-box">
+                            <img src="img/our-services/1-3.jpg" alt="Awesome Image" />
+                            <div class="overlay">
+                                <div class="box">
+                                    <div class="box-content">
+                                        <a href="material.html">Read More <i class="fa fa-angle-double-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <h3>Equipements et personnels</h3>
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo iure sunt amet fugiat facere nihil
+                            fugit! Saepe, dicta aperiam enim rem necessitatibus ea, corporis sit, tenetur illo quibusdam
+                            eum? Beatae.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="our-features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="single-our-feature">
+                        <div class="overlay" style="background-image: url(img/resources/single-featured-bg.jpg);">
+                            <div class="box">
+                                <div class="box-content">
+                                    <a href="#">Read More <i class="fa fa-angle-double-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="flaticon-factory"></i>
+                        </div>
+                        <h3>Expertise</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat reiciendis corporis veritatis,
+                            accusantium aliquam dolore voluptates.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-our-feature">
+                        <div class="overlay" style="background-image: url(img/resources/single-featured-bg.jpg);">
+                            <div class="box">
+                                <div class="box-content">
+                                    <a href="#">Our Team <i class="fa fa-angle-double-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="flaticon-people"></i>
+                        </div>
+                        <h3>Le savoir faire</h3>
+                        <p>Tenetur architecto dolorem exercitationem dolorum voluptatem quidem facere quae officiis quisquam
+                            itaque et eius!
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-our-feature">
+                        <div class="overlay" style="background-image: url(img/resources/single-featured-bg.jpg);">
+                            <div class="box">
+                                <div class="box-content">
+                                    <a href="#">Read More <i class="fa fa-angle-double-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="flaticon-medal"></i>
+                        </div>
+                        <h3>Expertise</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ab modi consectetur autem tempore
+                            nulla magni quaerat?
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact-section section-padding">
+        <div class="container">
+            <div class="section-title">
+                <h2><span>Contact</span></h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="large">Nous sommes disponibles pour vous servir.</p>
+                    <p><span class="bold">Pour vos requêtes:</span>
+                        Remplissez ce formulaire ou envoyez un email a l'adresse <a href="mailto:contact@geocose.com">contact@geocose.com</a> 
+                        et nous vous repondrons dans les brefs délais, ou appelez au
+                        <span class="highlight">(+243) 99999999.</span></p>
+                </div>
+                <div class="col-md-6">
+                    <form action="inc/sendemail.php" class="home-contact-form contact-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="text" name="name" placeholder="Votre nom *">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" name="email" placeholder="Votre adresse mail *">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" name="phone" placeholder="Telephone">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" name="solution" placeholder="Entreprise">
+                            </div>
+                            <div class="col-md-12">
+                                <textarea name="requirement" placeholder="Votre message *"></textarea>
+                                <button type="submit">Envoyer</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
